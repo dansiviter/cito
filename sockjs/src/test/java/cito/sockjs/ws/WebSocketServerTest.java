9f959c31706704de878e618d91c4a2e8c1ef25fd
Daniel Siviter <dansiviter@gmail.com>
2017-02-19 10:18:10 +0000

    Adding copyright notice

--- sockjs/src/test/java/cito/sockjs/ws/WebSocketServerTest.java
+++ sockjs/src/test/java/cito/sockjs/ws/WebSocketServerTest.java
@@ -1,15 +1,24 @@
+/*
+ * Copyright 2016-2017 Daniel Siviter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cito.sockjs.ws;
 
-import java.awt.Frame;
-
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.Test;
-import org.wildfly.swarm.undertow.WARArchive;
 
 import cito.sockjs.AbstractTest;
-import cito.sockjs.Headers;
 import cito.sockjs.WebSocketServer;
 
 /**
